0e63a5730745f97bafab272c74c126fbafab91a1
Gustavo A. Herrera <Gustavo.Herrera@levinglobal.com>
2020-02-11 15:22:41 -0300

    initial

---
+++ MiniMural/StickyNote.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniMural
+{
+    [DataContract]
+    public class StickyNote : BindableBase
+    {
+        public StickyNote(string txt, System.Drawing.Color color)
+        {
+            Text = txt;
+            Color = color;
+        }
+
+        private string mText;
+        private System.Drawing.Color mColor;
+
+        [DataMember]
+        public string Text
+        {
+            get
+            {
+                return mText;
+            }
+            set
+            {
+                SetProperty(ref mText, value, nameof(Text));
+            }
+        }
+
+        [DataMember]
+        public System.Drawing.Color Color {
+            get
+            {
+                return mColor;
+            }
+            set
+            {
+                SetProperty(ref mColor, value, nameof(Color));
+            }
+        }
+    }
+}
